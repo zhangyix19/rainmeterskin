ca66d9c08ab6150f5bb10e45cd517a5e6aecad4c
zhangyix19 <zhangyix19@mails.tsinghua.edu.cn>
2023-03-02 09:18:45 +0800

    myscheduler

--- Skins/Adv Schedule/Adv Schedule.lua
+++ Skins/Adv Schedule/Adv Schedule.lua
@@ -29,20 +29,29 @@ function Initialize()
 	iBH=tonumber(SKIN:GetVariable('BlockHeight'))
 	iBI=tonumber(SKIN:GetVariable('BlockInterval'))
 	iCP=tonumber(SKIN:GetVariable('Center'))
+	mainHeight=tonumber(SKIN:GetVariable('mainHeight'))
 	iBWI=iBW+iBI
 	iBHI=iBH+iBI
+	-- s(string) S(section) S(start)
 	sWN={}
 	sSP={}
 	sSN={}
 	sSS={}
 	sSE={}
+	sSstr={}
 	iSS={}
-	iSE={}
+	iSE={} -- intSectionEndtime
 	sCP={}
 	sCN={}
-	iCW={}
+	iCW={} -- weekday of the class
+	sCS={}
+	sCE={}
 	iCS={}
-	iCE={}
+	iCE={} -- intClassEndtime(last Version: intClassEndIndex)
+	sectionStartHeightOffset={}
+	sectionEndHeightOffset={}
+	classStartHeightOffset={}
+	classEndHeightOffset={}
 	for i=1,7 do
 		sWN[i]=tostring(SKIN:GetVariable('Weekday'..i))
 		SKIN:Bang(bSO,'MeterW'..i,'W',iBW)
@@ -55,22 +64,40 @@ function Initialize()
 		sSN[iSN]=string.match(sSP[iSN],'(.*)%.%.%d*:%d*%.%.%d*:%d*')
 		sSS[iSN]=string.match(sSP[iSN],'.*%.%.(%d*:%d*)%.%.%d*:%d*')
 		sSE[iSN]=string.match(sSP[iSN],'.*%.%.%d*:%d*%.%.(%d*:%d*)')
+		sSstr[iSN]=sSS[iSN]..'--'..sSE[iSN]
 		iSS[iSN]=tonumber(string.match(sSS[iSN],'(%d*):%d*')*60+string.match(sSS[iSN],'%d*:(%d*)'))
 		iSE[iSN]=tonumber(string.match(sSE[iSN],'(%d*):%d*')*60+string.match(sSE[iSN],'%d*:(%d*)'))
 		SKIN:Bang(bSO,'MeterS'..iSN,'Text',sSN[iSN])
 	end
+	totaltime=iSE[iSN]-iSS[1]
+	for i=1,iSN do
+		sectionStartHeightOffset[i]=(iSS[i]-iSS[1])/totaltime*mainHeight
+		sectionEndHeightOffset[i]=(iSE[i]-iSS[1])/totaltime*mainHeight
+	end
 	SKIN:Bang(bSO,'MeterT','H',iSN*iBHI-iBI)
 	iCN=0
-	iWM=0
+	iWM=0 -- int weekday max(if there is no class on Sun, then it =6 and there will not be Sun)
 	while (type(SKIN:GetVariable('Class'..iCN+1))~='nil') do
 		iCN=iCN+1
 		sCP[iCN]=tostring(SKIN:GetVariable('Class'..iCN))
-		sCN[iCN]=string.match(sCP[iCN],'(.*)%.%.%d*%.%.%d*%.%.%d*')
-		iCW[iCN]=tonumber(string.match(sCP[iCN],'.*%.%.(%d*)%.%.%d*%.%.%d*'))
+		if string.match(sCP[iCN],'.*%.%.%d*%.%.%d*:%d*%.%.%d*:%d*')==nil then
+			sectionofclass=tonumber(string.match(sCP[iCN],'.*%.%.%d*%.%.(%d)'))
+			sCP[iCN]=string.match(sCP[iCN],'(.*%.%.%d*%.%.)%d')..sSS[sectionofclass]..'..'..sSE[sectionofclass]
+		end
+		sCN[iCN]=string.match(sCP[iCN],'(.*)%.%.%d*%.%.%d*:%d*%.%.%d*:%d*')
+		iCW[iCN]=tonumber(string.match(sCP[iCN],'.*%.%.(%d*)%.%.%d*:%d*%.%.%d*:%d*'))
 		if iCW[iCN]>iWM then iWM=iCW[iCN] end
-		iCS[iCN]=tonumber(string.match(sCP[iCN],'.*%.%.%d*%.%.(%d*)%.%.%d*'))
-		iCE[iCN]=tonumber(string.match(sCP[iCN],'.*%.%.%d*%.%.%d*%.%.(%d*)'))
+		-- get string of start time and end time of each class
+		sCS[iCN]=string.match(sCP[iCN],'.*%.%.%d*%.%.(%d*:%d*)%.%.%d*:%d*')
+		sCE[iCN]=string.match(sCP[iCN],'.*%.%.%d*%.%.%d*:%d*%.%.(%d*:%d*)')
+		-- cal int by string
+		iCS[iCN]=tonumber(string.match(sCS[iCN],'(%d*):%d*')*60+string.match(sCS[iCN],'%d*:(%d*)'))
+		iCE[iCN]=tonumber(string.match(sCE[iCN],'(%d*):%d*')*60+string.match(sCE[iCN],'%d*:(%d*)'))
 		SKIN:Bang(bSO,'MeterC'..iCN,'Text',sCN[iCN])
+	end
+	for i=1,iCN do
+		classStartHeightOffset[i]=(iCS[i]-iSS[1])/totaltime*mainHeight
+		classEndHeightOffset[i]=(iCE[i]-iSS[1])/totaltime*mainHeight
 	end
 	sBP=tostring(SKIN:GetVariable('CURRENTPATH'))..'Blocks.inc'
 	fB=io.open(sBP,'r')
@@ -126,21 +153,23 @@ function Initialize()
 		SKIN:Bang(bSO,'MeterT','LeftMouseUpAction',bCM..' MeasureScript M()')
 	end
 	iT=iM
-	iW=0
-	iC=0
+	iW=0 -- int weekday last update
+	iC=0 -- int time last update
 end
 
 function Update()
-	iWN=msWN:GetValue()
+	iWN=msWN:GetValue() -- intweekday
 	if iWN==0 then iWN=7 end
-	sT=msT:GetStringValue()
+	sT=msT:GetStringValue() -- stringtime
 	if iBR==0 then
 		if iM==iT then
+			-- update every min
 			if (iW~=iWN)or(iC~=tonumber(string.match(sT,'(%d*):%d*')*60+string.match(sT,'%d*:(%d*)'))) then
+				-- update operations
 				iW=iWN
 				iC=tonumber(string.match(sT,'(%d*):%d*')*60+string.match(sT,'%d*:(%d*)'))
 				if iWN>iWM then
-					iS=1
+					iS=1 -- Main weekday
 				else
 					iS=iW
 				end
@@ -174,38 +203,49 @@ function U()
 		SKIN:Bang(bUMT,'MeterW'..i)
 	end
 	for i=1,iSN do
-		SKIN:Bang(bMM,iSW/2+iSH*(1-iM/iMM),iBH/2+(i+1)*iBHI,'MeterS'..i)
+		SKIN:Bang(bMM,iSW/2+iSH*(1-iM/iMM),(sectionEndHeightOffset[i]-sectionStartHeightOffset[i])/2+(2)*iBHI+sectionStartHeightOffset[i],'MeterS'..i)
+		SKIN:Bang(bSO,'MeterS'..i,'H',sectionEndHeightOffset[i]-sectionStartHeightOffset[i])
 	end
 	SKIN:Bang(bHM,'MeterB')
 	for i=1,iCN do
-		SKIN:Bang(bMM,iSW+iBI+iBW/2+(iCW[i]-1)*iBWI*iM/iMM+iSH*(1-iM/iMM),(iCS[i]+1)*iBHI+(iBH+(iCE[i]-iCS[i])*iBHI)/2,'MeterC'..i)
-		SKIN:Bang(bMM,iSW+iBI+(iCW[i]-1)*iBWI*iM/iMM+iSH*(1-iM/iMM),(iCS[i]+1)*iBHI,'MeterB'..i)
-		SKIN:Bang(bSO,'MeterB'..i,'H',iBH+(iCE[i]-iCS[i])*iBHI)
+		-- set height, offset of MeterC(lass)
+		SKIN:Bang(bMM,iSW+iBI+iBW/2+(iCW[i]-1)*iBWI*iM/iMM+iSH*(1-iM/iMM),2*iBHI+(classEndHeightOffset[i]-classStartHeightOffset[i])/2+classStartHeightOffset[i],'MeterC'..i)
+		SKIN:Bang(bSO,'MeterC'..i,'H',classEndHeightOffset[i]-classStartHeightOffset[i])
+		SKIN:Bang(bMM,iSW+iBI+(iCW[i]-1)*iBWI*iM/iMM+iSH*(1-iM/iMM),2*iBHI+classStartHeightOffset[i],'MeterB'..i)
+		SKIN:Bang(bSO,'MeterB'..i,'H',classEndHeightOffset[i]-classStartHeightOffset[i])
 		if iCW[i]~=iS then
 			SKIN:Bang(bSO,'MeterC'..i,'FontColor',sFC2S..','..sFC2A*iM/iMM)
 		else
 			SKIN:Bang(bSO,'MeterC'..i,'FontColor','#FontColor2#')
 		end
 		SKIN:Bang(bUMT,'MeterC'..i)
+		-- SKIN:Bang(bSO,'MeterDebug','Text',iC)
+		-- SKIN:Bang(bMM,500,500,'MeterDebug')
+		-- SKIN:Bang(bUMT,'MeterDebug')
 		if iS==iW then
 			if iCW[i]~=iS then
 				if iCW[i]<iS then
+					-- after main weekday use color3
 					SKIN:Bang(bSO,'MeterB'..i,'SolidColor',sSC3S..','..sSC3A*iM/iMM)
 				else
+					-- before main weekday use color2
 					SKIN:Bang(bSO,'MeterB'..i,'SolidColor',sSC2S..','..sSC2A*iM/iMM)
 				end
 			else
-				if iSE[iCE[i]]<=iC then
+				if iCE[i]<=iC then
+					-- the course's endtime < nowtime, done
 					SKIN:Bang(bSO,'MeterB'..i,'SolidColor','#SolidColor3#')
-				elseif iSS[iCS[i]]>iC then
+				elseif iCS[i]>iC then
+					-- the course's endtime < nowtime, todo
 					SKIN:Bang(bSO,'MeterB'..i,'SolidColor','#SolidColor2#')
 				else
-					iP=math.floor((iBH+(iCE[i]-iCS[i])*iBHI)*((iC-iSS[iCS[i]])/(iSE[iCE[i]]-iSS[iCS[i]])))
+					-- else, doing
+					iP=(iC-iCS[i])/totaltime*mainHeight
 					SKIN:Bang(bSO,'MeterB'..i,'SolidColor','#SolidColor3#')
 					SKIN:Bang(bSO,'MeterB'..i,'H',iP)
 					SKIN:Bang(bSM,'MeterB')
-					SKIN:Bang(bMM,iSW+iBI+(iCW[i]-1)*iBWI*iM/iMM+iSH*(1-iM/iMM),(iCS[i]+1)*iBHI+iP,'MeterB')
-					SKIN:Bang(bSO,'MeterB','H',iBH+(iCE[i]-iCS[i])*iBHI-iP)
+					SKIN:Bang(bMM,iSW+iBI+(iCW[i]-1)*iBWI*iM/iMM+iSH*(1-iM/iMM),2*iBHI+classStartHeightOffset[i]+iP,'MeterB')
+					SKIN:Bang(bSO,'MeterB','H',classEndHeightOffset[i]-classStartHeightOffset[i]-iP)
 					SKIN:Bang(bUMT,'MeterB')
 				end
 			end
